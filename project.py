f9d924918ff57d03e9872080bc059c9cc6d69ed5
Finn <just4studybro@gmail.com>
2026-08-25 21:00:26 -0500

    New file added

--- project.py
+++ project.py
@@ -1,7 +1,9 @@
 import os 
-from pathlib import Path   
+from pathlib import Path  
 
-categoria = {
+def run():
+
+        categoria = {
         ".jpg": "Imagenes",
         ".png": "Imagenes",
         ".jpeg": "Imagenes",
@@ -37,26 +39,28 @@ categoria = {
         ".go": "Codigo"
 }
 
-numero = 0
-carpet = Path.home() / "Downloads"
-print (Path.exists(carpet))
+        numero = 0
+        carpet = Path.home() / "Downloads"
+        print (Path.exists(carpet))
 
-Name = carpet.iterdir()
 
-for numero, Name in enumerate(carpet.iterdir(), start=1,):
-    print(numero, Name.name, Name.suffix)
+        Name = carpet.iterdir()
 
-    if Name.is_file() == False or not Name.suffix in categoria:
-        print("Esto no es un archivo o el archivo no se encontro")
-        continue
-    else:
-        print(categoria[Name.suffix])
-        destino = categoria[Name.suffix]
-        final = carpet / destino 
-        destino = final.mkdir(exist_ok=True)
-        Name.rename(final / Name.name)
+        for numero, Name in enumerate(carpet.iterdir(), start=1,):
+            print(numero, Name.name, Name.suffix)
 
-print("Archivos organizados correctamente")
+            if Name.is_file() == False or not Name.suffix in categoria:
+                print("Esto no es un archivo o el archivo no se encontro")
+                continue
+        
+        else:
+            print(categoria[Name.suffix])
+            destino = categoria[Name.suffix]
+            final = carpet / destino 
+            destino = final.mkdir(exist_ok=True)
+            Name.rename(final / Name.name)
+
+        print("Archivos organizados correctamente")
   
     
 
